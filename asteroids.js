56dc9f98efb589c1908235edc521577b979329ef
LucaSalgado <lucas.gi.salgado@gmail.com>
2021-04-11 13:15:09 -0300

    player atirando

--- asteroids.js
+++ asteroids.js
@@ -8,16 +8,24 @@ function Remap(x, in_min, in_max, out_min, out_max)
 }
                       
 //------------------------------------------------
-
-let gOri = 4; // ganho no ângulo de orientação
-let gainP = 3; // ganho na velocidade
+// variaveis globais
+const gOri = 4; // ganho no ângulo de orientação
+const gainP = 3; // ganho na velocidade
+const bMin = -120; // offset da borda minima do canvas
+const bMax = -80; // offset da borda maxima do canvas
 
 
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 480;
-canvas.height = 640;
+canvas.width = 480; // tamanho horizintal do canvas
+canvas.height = 640; // tamanho vertical do canvas
+
+let projeteis = []; // array de armazenamento dos projeteis
+const lmt = 5; // limite de "tempo de vida" de um projetil
+
+//-------------------------------------------------
+//classes 
 
 class Player {
     constructor (x, y, ori) {
@@ -67,39 +75,80 @@ class Player {
     moveFrente(){ // o desenho está em 90° em relação com a origem, então tive que adaptar a relação de navegação pelos quadrantes
         this.x += (Math.cos((this.ori+270)*Math.PI / 180)*gainP);
         this.y += (Math.sin((this.ori+270)*Math.PI / 180)*gainP);
+        // faz o player atravessar a tela
+        if(this.x < bMin){
+            this.x = canvas.width+bMax;
+        }else if(this.x > canvas.width+bMax){
+            this.x = bMin;
+        }
+        if(this.y < bMin){
+            this.y = canvas.height+bMax;
+        }else if(this.y > canvas.height+bMax){
+            this.y = bMin;
+        }
     }
 }
 
 class Projetil {
-    constructor(x, y, velocidade){
+    constructor(x, y, ori){
         this.x = x;
         this.y = y;
-        this.velocidade = velocidade;
+        this.ori = ori;
+        this.limite = 0;
     }
 
     draw(){
-        c.rect(this.x, this.y, 1, 1);
+
+        const cx = 101;// coordenada do centro da nave
+        const cy = 105;// coordenada do centro da nave
+
+        c.save();
+
+        c.translate(cx+this.x, cy+this.y);// move o centro da matriz para as coordenadas
+        c.rotate(this.ori*Math.PI / 180);// rotaciona a matriz de acordo com com o angulo
+        c.translate(-(cx+this.x), -(cy+this.y));// retorna o centro da matriz para a posição original
+
+        c.rect(this.x+100, this.y+60, 1, 1);
         c.strokeStyle = 'white';
         c.stroke();
+
+        c.restore();
+
     }
 
     update(){
         this.draw();
-        this.x += this.velocidade.x;
-        this.y += this.velocidade.y;
+        this.x += (Math.cos((this.ori+270)*Math.PI / 180)*(gainP+2));
+        this.y += (Math.sin((this.ori+270)*Math.PI / 180)*(gainP+2));
+
+        // faz o projetil atravessar a tela
+        if(this.x < bMin){
+            this.x = canvas.width+bMax;
+        }else if(this.x > canvas.width+bMax){
+            this.x = bMin;
+        }
+        if(this.y < bMin){
+            this.y = canvas.height+bMax;
+        }else if(this.y > canvas.height+bMax){
+            this.y = bMin;
+        }
+
+        this.limite += 0.05;
     }
 }
 
-let projeteis = [];
-
 function animate() {
-    requestAnimationFrame(animate);
-    projeteis.forEach((projetil) => {
+    //requestAnimationFrame(animate);
+    projeteis.forEach((projetil, index) => {
         projetil.update();
+        if(projetil.limite >= lmt)
+        {
+            projeteis.splice(index, 1); // remove os projeteis
+        }
     });
 }
 
-function mudaDirecao() {
+function acoes() {
 
     if (controller.ArrowLeft) {
         player.moveAntHorario();
@@ -108,6 +157,17 @@ function mudaDirecao() {
     }
     if (controller.ArrowUp) {
         player.moveFrente();
+    }
+    if (controller.c){
+
+       // if(projeteis.length < 5){
+            const px = player.x;
+            const py = player.y;
+            projeteis.push(
+                new Projetil(px, py, player.ori)
+            );
+            controller.c = false;
+       // }
     }
 }
 
@@ -127,9 +187,10 @@ function clearBoard() {
 function loopPrincipal() {
     //if (gameOver()) return;
     setTimeout (function onTick() {
-        mudaDirecao();
+        acoes();
         clearBoard();
         player.draw();
+        animate();
         loopPrincipal(); // repeat
     }, (1/60));
     
@@ -147,7 +208,7 @@ function run() {
     document.addEventListener("keyup", (e) => {
         if(controller[e.key])
         {
-            controller[e.key] = false  
+            controller[e.key] = false;  
         }
     });
     loopPrincipal();
